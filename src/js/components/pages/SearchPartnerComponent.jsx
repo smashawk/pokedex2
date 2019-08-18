b43868ac31bcb83b8ca04b787f39ea98f43cc363
smashawk <daichismash@gmail.com>
2019-08-18 21:25:53 +0900

    ✨ パートナー検索をCSSモジュール化した

--- src/js/components/pages/SearchPartnerComponent.jsx
+++ src/js/components/pages/SearchPartnerComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from '../../../scss/modules/SearchPartner.module.scss'
 // import axios from 'axios';
 import normalArray from '../../common/createNormalArray'
 
@@ -9,36 +10,35 @@ const SearchPartner = (props) => {
 
     return(
       
-      <section className="contentArea contentArea--searchPartner">
-        <div className="inputArea">
+      <section className={styles.contentArea}>
+        <div className={styles.inputArea}>
         <h2>3. あなたの相棒ポケモン検索</h2>
-          <p className="inputArea__desc">※ひらがな入力必須/フルネーム推奨</p>
-          <p className="inputArea__desc"></p>
-          <input 
+          <p className={styles.inputAreaDesc}>※ひらがな入力必須/フルネーム推奨</p>
+          <input
             id="inputName"
-            className="inputName"
+            className={styles.inputAreaName}
             type="text"
             placeholder="ひらがなで名前を入力しよう"
             />
           <button onClick={e => props.decidePartner(e)}>決定</button>
         </div>
         {props.isDecision && 
-          <div className="outputArea">
+          <div className={styles.outputArea}>
             <div>
-              <p className="partnerText"><span className="partnerSubText">{props.inputName}</span>は<span className="partnerSubText">{normalArray[props.partnerNo].name}</span>にきめた！</p>
+              <p className={styles.partnerText}><span className={styles.partnerSubText}>{props.inputName}</span>は<span className={styles.partnerSubText}>{normalArray[props.partnerNo].name}</span>にきめた！</p>
               <div>
-                <dl className="outputArea__list">
-                  <dt className="outputArea__title">図鑑番号</dt>
-                  <dd className="outputArea__desc">{normalArray[props.partnerNo].no}</dd>
-                  <dt className="outputArea__title">名前</dt>
-                  <dd className="outputArea__desc">{normalArray[props.partnerNo].name}</dd>
-                  <dt className="outputArea__title">タイプ</dt>
-                  <dd className="outputArea__desc">
-                    <span className="outputArea__type">{normalArray[props.partnerNo].types[0]}</span>
-                    <span className="outputArea__type">{normalArray[props.partnerNo].types[1]}</span>
+                <dl className={styles.outputAreaList}>
+                  <dt className={styles.outputAreaTitle}>図鑑番号</dt>
+                  <dd className={styles.outputAreaDesc}>{normalArray[props.partnerNo].no}</dd>
+                  <dt className={styles.outputAreaTitle}>名前</dt>
+                  <dd className={styles.outputAreaDesc}>{normalArray[props.partnerNo].name}</dd>
+                  <dt className={styles.outputAreaTitle}>タイプ</dt>
+                  <dd className={styles.outputAreaDesc}>
+                    <span className={styles.outputAreaType}>{normalArray[props.partnerNo].types[0]}</span>
+                    <span className={styles.outputAreaType}>{normalArray[props.partnerNo].types[1]}</span>
                   </dd>
                 </dl>
-                <div className={`outputArea__img outputArea__img--imgNo${props.partnerNo}`}></div>
+                <div className={`${styles.outputAreaImg} outputAreaImgNo${props.partnerNo}`}></div>
               </div>
             </div>
           </div>
