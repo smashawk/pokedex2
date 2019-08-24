d5afb8e17296cfb33d63453cbfad077a7429eab7
smashawk <daichismash@gmail.com>
2019-08-25 00:03:09 +0900

    🔧 タイプ検索のスタイルをstyled-componentsにした

--- src/js/components/pages/SearchTypeComponent/index.jsx
+++ src/js/components/pages/SearchTypeComponent/index.jsx
@@ -1,14 +1,84 @@
 import React from "react";
-import styles from '../../../../scss/modules/SearchType.module.scss'
+import styled from 'styled-components';
 import normalArray from '../../../common/createNormalArray'
 import TypeList from './typeList'
 
+const ContentArea = styled.section`
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const InputArea = styled.div`
+  margin: 0;
+  width: 220px;
+`;
+
+const InputAreaResetType = styled.button`
+  margin-top: 20px;
+`;
+
+const OutputArea = styled.div`
+  width: 550px;
+`;
+
+const OutputAreaList = styled.dl`
+  display: inline-block;
+  width: 330px;
+  margin-bottom: 0;
+  vertical-align: middle;
+`;
+
+const OutputAreaTitle = styled.dt`
+  display: inline-block;
+  width: 150px;
+`;
+
+const OutputAreaDesc = styled.dd`
+  display: inline-block;
+  width: 150px;
+`;
+
+const OutputAreaType = styled.span`
+  margin-left: 10px;
+`;
+
+const OutputAreaImg = styled.div`
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  vertical-align: middle;
+  ${props => {
+    if(props.id < 10) {
+      return`
+        background: center / contain no-repeat url(./images/00${props.id}_0.png);
+      `
+    } else if(9 < props.id && props.id < 100) {
+      return`
+        background: center / contain no-repeat url(./images/0${props.id}_0.png);
+      `
+    } else if(props.id < 1000) {
+      return`
+        background: center / contain no-repeat url(./images/${props.id}_0.png);
+      `
+    }
+  }}
+`;
+
+const TypeIconList = styled.ul`
+  width: 1120px;
+  margin-top: 30px;
+`;
+
+
 const SearchType = (props) => {
-  
+
   return(
     
-    <section className={styles.contentArea}>
-      <div className={styles.inputArea}>
+    <ContentArea>
+      <InputArea>
         <h2>2. タイプ検索</h2>
         <select id="typeSelector1" onChange={e => props.decideType(e)}>
           <option>-</option>
@@ -52,34 +122,34 @@ const SearchType = (props) => {
           <option>はがね</option>
           <option>フェアリー</option>
         </select>
-      <button onClick={e => props.resetType(e)} className={styles.inputAreaResetType}>タイプリセット</button>
-    </div>
+      <InputAreaResetType onClick={e => props.resetType(e)}>タイプリセット</InputAreaResetType>
+    </InputArea>
 
-    <div className={styles.outputArea}>
-        <dl className={styles.outputAreaList}>
-          <dt className={styles.outputAreaTitle}>図鑑番号</dt>
-          <dd className={styles.outputAreaDesc}>{props.id}</dd>
-          <dt className={styles.outputAreaTitle}>名前</dt>
-          <dd className={styles.outputAreaDesc}>{normalArray[props.id].name}</dd>
-          <dt className={styles.outputAreaTitle}>タイプ</dt>
-          <dd className={styles.outputAreaDesc}>
-            <span className={styles.outputAreaType}>{normalArray[props.id].types[0]}</span>
-            <span className={styles.outputAreaType}>{normalArray[props.id].types[1]}</span>
-          </dd>
-        </dl>
-        <div className={`${styles.outputAreaImg} outputAreaImgNo${props.id}`}></div>
-      </div>
+    <OutputArea>
+      <OutputAreaList>
+          <OutputAreaTitle>図鑑番号</OutputAreaTitle>
+          <OutputAreaDesc>{props.id}</OutputAreaDesc>
+          <OutputAreaTitle>名前</OutputAreaTitle>
+          <OutputAreaDesc>{normalArray[props.id].name}</OutputAreaDesc>
+          <OutputAreaTitle>タイプ</OutputAreaTitle>
+          <OutputAreaDesc>
+            <span>{normalArray[props.id].types[0]}</span>
+            <OutputAreaType>{normalArray[props.id].types[1]}</OutputAreaType>
+          </OutputAreaDesc>
+      </OutputAreaList>
+      <OutputAreaImg id={props.id} />
+    </OutputArea>
      
      <div>
        {/* {props.clickMessage && <p>ポケモンをクリック！</p>} */}
-       <ul className={styles.typeIconList}>
+       <TypeIconList>
          <TypeList
           typeArray={props.typeArray}
           showData={props.showData}
          />
-       </ul>
+       </TypeIconList>
      </div>
-   </section>
+    </ContentArea>
 
   )
 }
--- src/js/components/pages/SearchTypeComponent/typeList.jsx
+++ src/js/components/pages/SearchTypeComponent/typeList.jsx
@@ -1,5 +1,34 @@
 import React from "react";
-import styles from '../../../../scss/modules/SearchType.module.scss'
+import styled from 'styled-components';
+
+
+const TypeIconListImg = styled.li`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  margin: 0 auto;
+  vertical-align: middle;
+  cursor: pointer;
+  ${props => {
+
+    if(props.id < 10) {
+      return`
+        background: center / 40px 40px no-repeat url(./images/00${props.id}_4.png);
+      `
+    } else if(9 < props.id && props.id < 100) {
+      return`
+        background: center / 40px 40px no-repeat url(./images/0${props.id}_4.png);
+      `
+    } else if(props.id < 1000) {
+      return`
+        background: center / 40px 40px no-repeat url(./images/${props.id}_4.png);
+      `
+    }
+  }}
+  &:hover {
+    background-size: contain;
+  }
+`;
 
 // typeArrayのポケモンDOMを作っておく
 const typeList = props => {
@@ -7,14 +36,12 @@ const typeList = props => {
   const nodes = props.typeArray.map(value => {
 
     const node = 
-    <li 
+    <TypeIconListImg 
       key={value.key}
       id={value.number.no}
-      className={`${styles.typeIconListImg} typeIconListImgNo${value.number.no}`}
-      onMouseOver={e => props.showData(e.target.id)}
-      // onClick={e => props.showData(e.target.id)}
-    >
-    </li>;
+      // onMouseOver={e => props.showData(e.target.id)}
+      onClick={e => props.showData(e.target.id)}
+    />;
   
     return node;
   });
