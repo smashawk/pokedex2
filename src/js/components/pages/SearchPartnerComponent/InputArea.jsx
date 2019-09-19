3fb1dd713ae36b9a7758437e29b9803fae19fc4c
朝長　大地 <d_tomonaga@waku-2.com>
2019-09-19 23:32:17 +0900

    テキスト追加

--- src/js/components/pages/SearchPartnerComponent/InputArea.jsx
+++ src/js/components/pages/SearchPartnerComponent/InputArea.jsx
@@ -5,7 +5,7 @@ import * as styles from '../../../../styles/baseStyle';
 
 const InputAreaWrap = styled.div``;
 
-// const InputAreaDesc = styled(styles.BaseInputAreaDesc)``;
+const InputAreaDesc = styled(styles.BaseInputAreaDesc)``;
 
 const InputAreaText = styled(styles.BaseInputAreaText)`
   width: 230px
@@ -18,7 +18,7 @@ const InputArea = (props) => {
 
     <InputAreaWrap>
       <h2>3. あなたの相棒ポケモン検索</h2>
-        {/* <InputAreaDesc>※ひらがな入力必須/フルネーム推奨</InputAreaDesc> */}
+        <InputAreaDesc>※フルネーム推奨ですが、何入力してもいいです</InputAreaDesc>
         <InputAreaText 
           id="inputName"
           type="text"
