0f72df511354664769012ccfd5f97e6db37a986a
smashawk <daichismash@gmail.com>
2019-09-22 18:13:25 +0900

    🔧 変数名変更

--- src/js/actions/SearchPoke/decidePoke.ts
+++ src/js/actions/SearchPoke/decidePoke.ts
@@ -2,19 +2,19 @@ import normalArray from '../../common/createNormalArray'
 import { DecidePokeActionTypes, SEARCH_POKE_DATA } from '../../types/SearchPoke/decidePokeTypes'
 
 
-export const decidePoke = (text:number | string):DecidePokeActionTypes => {
+export const decidePoke = (inputText:number | string):DecidePokeActionTypes => {
 
   let no;
   let errorMessage;
 
   // 何も入力されていない場合 → No.0のおばけ画像を表示
-  if(!text) {
+  if(!inputText) {
     no = 0;
     errorMessage = false;
   
   // 範囲内の数字が入力されている場合
-  } else if(text < 803 && 0 < text) {
-    no = text as number;
+  } else if(inputText < 803 && 0 < inputText) {
+    no = inputText as number;
     errorMessage = false;
 
   // 範囲外の数字、及びその他無関係な文字列が入力されている場合
@@ -25,7 +25,7 @@ export const decidePoke = (text:number | string):DecidePokeActionTypes => {
 
   // ポケモンの名前が入力されている場合
   for(let i = 0; i<normalArray.length; i++) {
-    if(text === normalArray[i].name) {
+    if(inputText === normalArray[i].name) {
       no = i;
       errorMessage = false;
       break;
--- src/js/containers/SearchPokeContainer.jsx
+++ src/js/containers/SearchPokeContainer.jsx
@@ -10,7 +10,7 @@ const mapStateToProps = state => ({
 
 
 const mapDispatchToProps = dispatch => ({
-  decidePoke: text => dispatch(decidePoke(text)),
+  decidePoke: inputText => dispatch(decidePoke(inputText)),
 });
 
 
