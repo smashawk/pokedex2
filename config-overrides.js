b02d2033fd5decbb4f2d091f9448e86fd16445a3
smashawk <daichismash@gmail.com>
2020-10-23 22:31:02 +0900

    webpackのpathsを追加し、絶対パス指定できるようにした

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const path = require('path')
+module.exports = (config) => {
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.alias,
+      '@js': path.resolve(__dirname, './src/js'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    }
+  };
+
+  return config;
+}
