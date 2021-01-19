fea01c19b386011c8f73ee4f1a1d5d3aa4960bb7
smashawk <daichismash@gmail.com>
2021-01-20 02:32:28 +0900

    feat: atoms/FixButton作成

---
+++ src/components/atoms/FixButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, ButtonProps } from "@material-ui/core";
+
+type OwnProps = {
+	color?: ButtonProps["color"];
+	text: string;
+	variant?: ButtonProps["variant"];
+	onClick: () => void;
+};
+
+type Props = OwnProps;
+
+export const FixButton = ({
+	color,
+	text,
+	variant,
+	onClick
+}: Props): JSX.Element => {
+	return (
+		<Button color={color} variant={variant} onClick={onClick}>
+			{text}
+		</Button>
+	);
+};
