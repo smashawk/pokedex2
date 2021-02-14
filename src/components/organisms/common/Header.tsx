e79f36f3e10cdcd4752e43702df3453bcec747ad
smashawk <daichismash@gmail.com>
2021-02-15 01:14:02 +0900

    refactor: Header

--- src/components/organisms/common/Header.tsx
+++ src/components/organisms/common/Header.tsx
@@ -1,50 +1,9 @@
-import {
-	createMuiTheme,
-	createStyles,
-	makeStyles
-} from "@material-ui/core/styles";
+import { AppBar, Tab, Tabs } from "@material-ui/core";
 import { Link, useLocation } from "react-router-dom";
-import { Paper, Tab, Tabs } from "@material-ui/core";
-
-export const theme = createMuiTheme({
-	palette: {
-		text: {
-			primary: "#000"
-		}
-	},
-	typography: {
-		htmlFontSize: 10,
-		h2: {
-			fontSize: 24,
-			fontWeight: 700
-		},
-		body1: {
-			fontSize: 20
-		}
-	}
-});
-
-const useStyles = makeStyles(() =>
-	createStyles({
-		root: {
-			flexGrow: 1
-		},
-		mainWrapper: {
-			textAlign: "center"
-			// margin: theme.spacing(10, "auto")
-		},
-		toolBar: {
-			justifyContent: "center"
-		}
-	})
-);
 
 export const Header = (): JSX.Element => {
-	const classes = useStyles();
-
 	const currentTab = useLocation().pathname;
-
-	const menuInfo = [
+	const menuList = [
 		{
 			label: "TOP",
 			value: "/",
@@ -61,34 +20,32 @@ export const Header = (): JSX.Element => {
 			id: 2
 		},
 		{
-			label: "相棒ポケモン検索",
+			label: "相棒検索",
 			value: "/partner",
 			id: 3
 		}
 	];
 
 	return (
-		<header>
-			<Paper className={classes.root}>
-				<Tabs
-					value={currentTab}
-					indicatorColor="primary"
-					textColor="primary"
-					centered
-				>
-					{menuInfo.map((item) => {
-						return (
-							<Tab
-								key={item.id}
-								label={item.label}
-								value={item.value}
-								to={item.value}
-								component={Link}
-							/>
-						);
-					})}
-				</Tabs>
-			</Paper>
-		</header>
+		<AppBar position="static" color="primary">
+			<Tabs
+				value={currentTab}
+				indicatorColor="secondary"
+				textColor="inherit"
+				centered
+			>
+				{menuList.map((item) => {
+					return (
+						<Tab
+							key={item.id}
+							label={item.label}
+							value={item.value}
+							to={item.value}
+							component={Link}
+						/>
+					);
+				})}
+			</Tabs>
+		</AppBar>
 	);
 };
