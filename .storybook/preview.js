ee5f4a66989c442d22d590de639a0e89426e542e
smashawk <daichismash@gmail.com>
2021-02-24 02:30:46 +0900

    feat: add global css in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import { theme } from "../src/styles/theme";
+
+export const decorators = [
+	(Story) => (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Story />
+		</ThemeProvider>
+	)
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+	actions: { argTypesRegex: "^on[A-Z].*" }
+};
