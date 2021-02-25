48bb63209b6584cf70ac785224b582421c4a078d
smashawk <daichismash@gmail.com>
2021-02-26 02:15:38 +0900

    feat: add loading img in DataCard

--- src/components/atoms/PokeImg.tsx
+++ src/components/atoms/PokeImg.tsx
@@ -1,27 +1,30 @@
-import { VFC } from "react";
+import { useEffect, useState, VFC } from "react";
 import { createStyles, makeStyles, Box } from "@material-ui/core";
 import classNames from "classnames";
 
 const useStyles = makeStyles(() =>
 	createStyles({
-		pokeImg: {
+		root: {
 			width: 100,
 			height: 100,
-			margin: "auto",
-			backgroundSize: "contain",
-			backgroundPosition: "center",
-			backgroundRepeat: "no-repeat",
+			margin: "auto"
+		},
+		pokeImg: {
+			display: "none",
+			width: "auto",
+			height: "100%",
 			filter: "drop-shadow(2px 4px 6px black)"
 		},
-		pokeImgHome: (no) => ({
-			backgroundImage: `url(https://www.cpokemon.com/pokes/home/${no}.png)`
-		}),
-		pokeImgDotGif: (no) => ({
-			backgroundImage: `url(https://www.cpokemon.com/pokes/animated/ds/${no}.gif)`
-		}),
-		pokeImgAnimeGif: (no) => ({
-			backgroundImage: `url(https://www.cpokemon.com/pokes/animated/3ds/${no}.gif)`
-		})
+		loadingImg: {
+			display: "block",
+			width: "50%",
+			height: "75%",
+			margin: "0 auto",
+			paddingTop: "25px"
+		},
+		pokeImgLoading: {
+			display: "block"
+		}
 	})
 );
 
@@ -30,19 +33,43 @@ export type Props = {
 };
 
 export const PokeImg: VFC<Props> = ({ no }) => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setLoading(true);
+	}, [no]);
+	const imageLoaded = () => {
+		setLoading(false);
+	};
+
+	let url = "";
+
+	if (no > 0 && no < 650)
+		url = `https://www.cpokemon.com/pokes/animated/ds/${no}.gif`;
+	else if (no > 649 && no < 793)
+		url = `https://www.cpokemon.com/pokes/animated/3ds/${no}.gif`;
+	else url = `https://www.cpokemon.com/pokes/home/${no}.png`;
+
 	const classes = useStyles(no);
 
 	return (
-		<>
-			{no > 0 && no < 650 && (
-				<Box className={classNames(classes.pokeImg, classes.pokeImgDotGif)} />
+		<Box className={classes.root}>
+			{(no === 0 || loading) && (
+				<img
+					className={classNames(classes.pokeImg, loading && classes.loadingImg)}
+					alt=""
+					src={`${process.env.PUBLIC_URL}/images/monsterball.png`}
+				/>
 			)}
-			{no > 649 && no < 793 && (
-				<Box className={classNames(classes.pokeImg, classes.pokeImgAnimeGif)} />
-			)}
-			{no > 792 && (
-				<Box className={`${classes.pokeImg} ${classes.pokeImgHome}`} />
-			)}
-		</>
+			<img
+				className={classNames(
+					classes.pokeImg,
+					!loading && classes.pokeImgLoading
+				)}
+				onLoad={imageLoaded}
+				alt=""
+				src={url}
+			/>
+		</Box>
 	);
 };
