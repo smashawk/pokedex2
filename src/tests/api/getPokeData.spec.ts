3601811edff00943f3c616a922f2a97b1cec7a7c
obake-fe <daichismash@gmail.com>
2021-03-01 00:22:53 +0900

    refactor: rewrite test PokeDataAPI

--- src/tests/api/getPokeData.spec.ts
+++ src/tests/api/getPokeData.spec.ts
@@ -1,29 +1,15 @@
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
 import { getPokeData } from "@api/requests/getPokeData";
-import mockPokeData from "./mocks/mockPokeData.json";
 
 describe("getPokeData", () => {
-	const mock = new MockAdapter(axios);
-
-	afterEach(() => {
-		mock.reset();
-	});
-
 	it("should succeed", async () => {
 		const no = 1;
-		mock
-			.onGet(`https://pokeapi.co/api/v2/pokemon/${no}`)
-			.reply(200, mockPokeData);
-
 		const PokeData = await getPokeData(no);
 
-		expect(PokeData.data.id).toBe(mockPokeData.id);
+		expect(PokeData.status).toBe(200);
 	});
 
 	it("should fail with nonexistent no", async () => {
 		const no = 0;
-		mock.onGet(`https://pokeapi.co/api/v2/pokemon/${no}`).reply(404, {});
 
 		try {
 			await getPokeData(no);
@@ -31,4 +17,55 @@ describe("getPokeData", () => {
 			expect(error.response.status).toBe(404);
 		}
 	});
+
+	// it("search nonexistent no 1~300", async () => {
+	// 	const noArray = [...Array(300).keys()].map((i) => i + 1);
+	//
+	// 	const data = await Promise.all(
+	// 		noArray.map(async (no) => {
+	// 			try {
+	// 				const response = await getPokeData(no);
+	// 				return { no, status: response.status };
+	// 			} catch (error) {
+	// 				return { no, status: error.response.status };
+	// 			}
+	// 		})
+	// 	);
+	//
+	// 	expect(data).toEqual(noArray.map((no) => ({ no, status: 200 })));
+	// });
+	//
+	// it("search nonexistent no 301~550", async () => {
+	// 	const noArray = [...Array(250).keys()].map((i) => i + 301);
+	//
+	// 	const data = await Promise.all(
+	// 		noArray.map(async (no) => {
+	// 			try {
+	// 				const response = await getPokeData(no);
+	// 				return { no, status: response.status };
+	// 			} catch (error) {
+	// 				return { no, status: error.response.status };
+	// 			}
+	// 		})
+	// 	);
+	//
+	// 	expect(data).toEqual(noArray.map((no) => ({ no, status: 200 })));
+	// });
+	//
+	// it("search nonexistent no 551~792", async () => {
+	// 	const noArray = [...Array(242).keys()].map((i) => i + 551);
+	//
+	// 	const data = await Promise.all(
+	// 		noArray.map(async (no) => {
+	// 			try {
+	// 				const response = await getPokeData(no);
+	// 				return { no, status: response.status };
+	// 			} catch (error) {
+	// 				return { no, status: error.response.status };
+	// 			}
+	// 		})
+	// 	);
+	//
+	// 	expect(data).toEqual(noArray.map((no) => ({ no, status: 200 })));
+	// });
 });
