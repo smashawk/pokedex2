4c372f4dec198d1d98cc7f912bbf9860918b21f8
obake-fe <daichismash@gmail.com>
2021-03-02 00:28:19 +0900

    refactor: utils test

--- src/tests/utils/decidePartnerNo.spec.ts
+++ src/tests/utils/decidePartnerNo.spec.ts
@@ -1,34 +1,19 @@
 import { decidePartnerNo } from "@utils/decidePartnerNo";
 
 describe("decidePartnerNo", () => {
-	it("obake_fe", () => {
-		expect(decidePartnerNo("obake_fe")).toBeGreaterThan(0);
-		expect(decidePartnerNo("obake_fe")).toBeLessThan(793);
+	it.each([
+		["obake_fe"],
+		["ひらがな"],
+		["カタカナ"],
+		["漢字"],
+		["数字123"],
+		["記号¥^-@[]/.;|~"],
+		["絵文字👻"]
+	])("decidePartnerNo", (name) => {
+		expect(decidePartnerNo(name)).toBeGreaterThan(0);
+		expect(decidePartnerNo(name)).toBeLessThan(793);
 	});
-	it("ひらがな", () => {
-		expect(decidePartnerNo("ひらがな")).toBeGreaterThan(0);
-		expect(decidePartnerNo("ひらがな")).toBeLessThan(793);
-	});
-	it("カタカナ", () => {
-		expect(decidePartnerNo("カタカナ")).toBeGreaterThan(0);
-		expect(decidePartnerNo("カタカナ")).toBeLessThan(793);
-	});
-	it("漢字", () => {
-		expect(decidePartnerNo("漢字")).toBeGreaterThan(0);
-		expect(decidePartnerNo("漢字")).toBeLessThan(793);
-	});
-	it("数字", () => {
-		expect(decidePartnerNo("数字123")).toBeGreaterThan(0);
-		expect(decidePartnerNo("数字123")).toBeLessThan(793);
-	});
-	it("記号", () => {
-		expect(decidePartnerNo("¥^-@[]/.;|~")).toBeGreaterThan(0);
-		expect(decidePartnerNo("¥^-@[]/.;|~")).toBeLessThan(793);
-	});
-	it("絵文字", () => {
-		expect(decidePartnerNo("👻")).toBeGreaterThan(0);
-		expect(decidePartnerNo("👻")).toBeLessThan(793);
-	});
+
 	it("792ぴったり", () => {
 		expect(decidePartnerNo("ddddddd\\")).toBeGreaterThan(0);
 		expect(decidePartnerNo("ddddddd\\")).toBeLessThan(793);
--- src/tests/utils/translateHiratoRoman.spec.ts
+++ src/tests/utils/translateHiratoRoman.spec.ts
@@ -1,70 +1,30 @@
 import { translateHiraToRoman } from "@utils/translateHiratoRoman";
 
 describe("translateHiraToRoman", () => {
-	it("ふしぎだね", () => {
-		expect(translateHiraToRoman("ふしぎだね")).toBe("fushigidane");
-	});
-	it("りざーどん", () => {
-		expect(translateHiraToRoman("りざーどん")).toBe("rizadon");
-	});
-	it("かめっくす", () => {
-		expect(translateHiraToRoman("かめっくす")).toBe("kamekkusu");
-	});
-	it("きゃたぴー", () => {
-		expect(translateHiraToRoman("きゃたぴー")).toBe("kyatapi");
-	});
-	it("ぴかちゅう", () => {
-		expect(translateHiraToRoman("ぴかちゅう")).toBe("pikachuu");
-	});
-	it("もるふぉん", () => {
-		expect(translateHiraToRoman("もるふぉん")).toBe("morufon");
-	});
-	it("けーしぃ", () => {
-		expect(translateHiraToRoman("けーしぃ")).toBe("kesyi");
-	});
-	it("しぇるだー", () => {
-		expect(translateHiraToRoman("しぇるだー")).toBe("sheruda");
-	});
-	it("ふぁいやー", () => {
-		expect(translateHiraToRoman("ふぁいやー")).toBe("faiya");
-	});
-	it("ぴぃ", () => {
-		expect(translateHiraToRoman("ぴぃ")).toBe("pyi");
-	});
-	it("えーふぃ", () => {
-		expect(translateHiraToRoman("えーふぃ")).toBe("efi");
-	});
-	it("ぽりごん２", () => {
-		expect(translateHiraToRoman("ぽりごん２")).toBe("porigon２");
-	});
-	it("せれびぃ", () => {
-		expect(translateHiraToRoman("せれびぃ")).toBe("serebixi");
-	});
-	it("ちぇりむ", () => {
-		expect(translateHiraToRoman("ちぇりむ")).toBe("cherimu");
-	});
-	it("ぽりごんＺ", () => {
-		expect(translateHiraToRoman("ぽりごんＺ")).toBe("porigonＺ");
-	});
-	it("でぃあるが", () => {
-		expect(translateHiraToRoman("でぃあるが")).toBe("dhiaruga");
-	});
-	it("うぉーぐる", () => {
-		expect(translateHiraToRoman("うぉーぐる")).toBe("uxoguru");
-	});
-	it("ふらーじぇす", () => {
-		expect(translateHiraToRoman("ふらーじぇす")).toBe("furajesu");
-	});
-	it("めぇーくる", () => {
-		expect(translateHiraToRoman("めぇーくる")).toBe("mexekuru");
-	});
-	it("すなばぁ", () => {
-		expect(translateHiraToRoman("すなばぁ")).toBe("sunabaxa");
-	});
-	it("たいぷ：ぬる", () => {
-		expect(translateHiraToRoman("たいぷ：ぬる")).toBe("taipu：nuru");
-	});
-	it("しるゔぁでぃ", () => {
-		expect(translateHiraToRoman("しるゔぁでぃ")).toBe("shiruvadhi");
+	it.each([
+		["ふしぎだね", "fushigidane"],
+		["りざーどん", "rizadon"],
+		["かめっくす", "kamekkusu"],
+		["きゃたぴー", "kyatapi"],
+		["ぴかちゅう", "pikachuu"],
+		["もるふぉん", "morufon"],
+		["けーしぃ", "kesyi"],
+		["しぇるだー", "sheruda"],
+		["ふぁいやー", "faiya"],
+		["ぴぃ", "pyi"],
+		["えーふぃ", "efi"],
+		["ぽりごん２", "porigon２"],
+		["せれびぃ", "serebixi"],
+		["ちぇりむ", "cherimu"],
+		["ぽりごんＺ", "porigonＺ"],
+		["でぃあるが", "dhiaruga"],
+		["うぉーぐる", "uxoguru"],
+		["ふらーじぇす", "furajesu"],
+		["めぇーくる", "mexekuru"],
+		["すなばぁ", "sunabaxa"],
+		["たいぷ：ぬる", "taipu：nuru"],
+		["しるゔぁでぃ", "shiruvadhi"]
+	])("translateHiraToRoman", (name, expectValue) => {
+		expect(translateHiraToRoman(name)).toBe(expectValue);
 	});
 });
