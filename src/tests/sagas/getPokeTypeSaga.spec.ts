dc3961770cd3ff38ec9f5a6c3098869c46083773
obake-fe <daichismash@gmail.com>
2021-03-03 02:50:47 +0900

    feat: add pokeTypeSaga test

---
+++ src/tests/sagas/getPokeTypeSaga.spec.ts
@@ -0,0 +1,89 @@
+import { expectSaga } from "redux-saga-test-plan";
+import pokeTypeGrass from "@api/mocks/pokeTypeGrass.mock.json";
+import { normalizePokeData } from "@store/getPokeTypeData/normalizer";
+import * as actions from "@store/getPokeTypeData/actions";
+import * as reducers from "@store/getPokeTypeData/reducers";
+import {
+	watchGetPokeType1Data,
+	watchGetPokeType2Data
+} from "@sagas/getPokeTypeSaga";
+
+describe("getPokeTypeSaga", () => {
+	describe("type1", () => {
+		const handler = jest.fn();
+
+		it("should succeeded", () => {
+			const type = "grass";
+			const normalizedPokeData = normalizePokeData(pokeTypeGrass);
+			handler.mockReturnValue(normalizedPokeData);
+
+			expectSaga(watchGetPokeType1Data, handler)
+				.withReducer(reducers.searchTypeGetPokeTypeDataReducer as never)
+				// 最終的に期待する結果
+				.put(actions.type1FetchSuccess(normalizedPokeData))
+				.dispatch(actions.type1FetchStarted(type))
+				.hasFinalState({
+					...reducers.initialState,
+					type1: { ...normalizedPokeData }
+				})
+				.run();
+		});
+
+		it("should failed with nonexistent no", () => {
+			const error = {
+				response: {
+					status: 404
+				}
+			};
+			const type = "error";
+			handler.mockReturnValue(error);
+
+			expectSaga(watchGetPokeType1Data, handler)
+				.withReducer(reducers.searchTypeGetPokeTypeDataReducer as never)
+				// 最終的に期待する結果
+				.put(actions.type1FetchFailed(error))
+				.dispatch(actions.type1FetchStarted(type))
+				.hasFinalState({ ...reducers.initialState })
+				.run();
+		});
+	});
+
+	describe("type2", () => {
+		const handler = jest.fn();
+
+		it("should succeeded", () => {
+			const type = "grass";
+			const normalizedPokeData = normalizePokeData(pokeTypeGrass);
+			handler.mockReturnValue(normalizedPokeData);
+
+			expectSaga(watchGetPokeType2Data, handler)
+				.withReducer(reducers.searchTypeGetPokeTypeDataReducer as never)
+				// 最終的に期待する結果
+				.put(actions.type2FetchSuccess(normalizedPokeData))
+				.dispatch(actions.type2FetchStarted(type))
+				.hasFinalState({
+					...reducers.initialState,
+					type2: { ...normalizedPokeData }
+				})
+				.run();
+		});
+
+		it("should failed with nonexistent no", () => {
+			const error = {
+				response: {
+					status: 404
+				}
+			};
+			const type = "error";
+			handler.mockReturnValue(error);
+
+			expectSaga(watchGetPokeType1Data, handler)
+				.withReducer(reducers.searchTypeGetPokeTypeDataReducer as never)
+				// 最終的に期待する結果
+				.put(actions.type2FetchFailed(error))
+				.dispatch(actions.type2FetchStarted(type))
+				.hasFinalState({ ...reducers.initialState })
+				.run();
+		});
+	});
+});
