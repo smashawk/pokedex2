99833b1f5779bcc5a77321ecd1cf7c22f3a51a45
obake-fe <daichismash@gmail.com>
2021-03-07 20:03:11 +0900

    style: switcher

--- src/components/atoms/Switcher.tsx
+++ src/components/atoms/Switcher.tsx
@@ -1,5 +1,27 @@
 import { ChangeEvent, VFC } from "react";
-import { Switch, FormControlLabel } from "@material-ui/core";
+import {
+	makeStyles,
+	createStyles,
+	Switch,
+	FormControlLabel
+} from "@material-ui/core";
+import { theme } from "@styles/theme";
+
+const useStyles = makeStyles(() =>
+	createStyles({
+		switchBase: {
+			color: theme.palette.primary.main,
+			"&$checked": {
+				color: theme.palette.secondary.main
+			},
+			"&$checked + $track": {
+				backgroundColor: theme.palette.secondary.main
+			}
+		},
+		checked: {},
+		track: { backgroundColor: theme.palette.primary.main }
+	})
+);
 
 export type Props = {
 	checked: boolean;
@@ -9,8 +31,14 @@ export type Props = {
 };
 
 export const Switcher: VFC<Props> = ({ checked, label, onChange, testId }) => {
+	const classes = useStyles();
 	const switchComp = (
 		<Switch
+			classes={{
+				switchBase: classes.switchBase,
+				checked: classes.checked,
+				track: classes.track
+			}}
 			checked={checked}
 			onChange={onChange}
 			color="primary"
